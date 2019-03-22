af8a0a8e9d25cf2008c1b695c840a0ca818dd707
Nicole Hategekimana <nicoleha127@gmail.com>
2019-03-22 12:52:45 +0200

    profile , image and comment class created

--- gramy/forms.py
+++ gramy/forms.py
@@ -1,7 +1,18 @@
-from .models import Profile
+from .models import Profile,Image,Comments
 from django import forms
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user']
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['user']
+
+class CommentsForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        exclude = ['user']
+
