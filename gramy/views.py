dfd41033c8d8606814a018b886c3bcc0180cff53
Nicole Hategekimana <nicoleha127@gmail.com>
2019-03-22 12:55:48 +0200

    folder templates created

--- gramy/views.py
+++ gramy/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http  import HttpResponse,HttpResponse
 from .forms import ProfileForm
+from .models import Image,Profile,User
 
 def home(request):
+    
     return render(request,'index.html')
+
+def myProfile(request):
+    profiles = Profile.objects.all()
+    return render(request,'my_profile.html',{"profiles":profiles})
 
 def profile(request):
     current_user = request.user
@@ -14,8 +20,34 @@ def profile(request):
             profile.user = current_user
             profile.save()
 
-        return redirect('welcome')
-
     else:
         form = ProfileForm()
     return render(request, 'profile.html', {"form": form})
+
+def image(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = current_user
+            image.save()
+
+    else:
+        form = ImageForm()
+    return render(request, 'image.html', {"form": form})
+
+def comments(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = CommentsForm(request.POST, request.FILES)
+        if form.is_valid():
+            comments = form.save(commit=False)
+            comments.user = current_user
+            comments.save()
+
+    else:
+        form = CommentsForm()
+    return render(request, 'comments.html', {"form": form})
+
+
