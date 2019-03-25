8e4a4691f72447b5bb7bac2820461ec225797e8b
Nicole Hategekimana <nicoleha127@gmail.com>
2019-03-25 08:54:11 +0200

    created a route to the login or sign in

--- gramy/urls.py
+++ gramy/urls.py
@@ -5,7 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$',views.home,name = 'welcome'),
-    url(r'^profile$', views.profile, name='profile'),
-    url(r'^myProfile$', views.myProfile, name='myProfile'),
-
+    url(r'^profile/', views.profile, name='profile'),
+    url(r'^myProfile/', views.myProfile, name='myProfile'),
+    url(r'^image/', views.image, name='image'),
+    url(r'^comments/', views.comments, name='comments'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
