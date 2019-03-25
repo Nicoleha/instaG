b225d4ef649d4f0f015391b8667f2f45e34702c8
Nicole Hategekimana <nicoleha127@gmail.com>
2019-03-25 08:49:06 +0200

    added a like class in models.py

--- gramy/models.py
+++ gramy/models.py
@@ -35,7 +35,7 @@ class Image(models.Model):
     name = models.CharField(max_length =60)
     caption = models.CharField(max_length =200)
     likes= models.IntegerField(default=0)
-    profile = models.ForeignKey(Profile)
+    profile = models.ForeignKey(Profile, null = True)
    
     def __str__(self):
         return self.name
